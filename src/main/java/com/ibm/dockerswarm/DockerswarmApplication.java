25ebaee6ebea36a51949e27fb91c1f450102b0bb
ibmdev <ibmdev102017@gmail.com>
2018-11-17 10:53:58 +0100

    Initial commit Spring Actuator

---
+++ src/main/java/com/ibm/dockerswarm/DockerswarmApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.dockerswarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerswarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerswarmApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ibm/dockerswarm/DockerswarmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ibm.dockerswarm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerswarmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
